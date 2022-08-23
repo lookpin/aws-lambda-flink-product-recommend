aaa70bb6f0770d259c6242930ced247de820a147
jongeunjeong <79628712+jongeunjeong@users.noreply.github.com>
2022-08-23 11:59:13 +0900

    print(event)

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -6,23 +6,12 @@ opensearch = OpensearchService()
 realtime_service.set_opensearch_service(opensearch)
 
 def lambda_handler(event, context):
-    # return {'event': event, 'context': context}
-    id = event['params']['path'].get('id', 0)  # coordi_id, product_id,
-    recommend_type = event['params']['querystring'].get('recommendType', None)
-    ids = event['params']['querystring'].get('ids', None)
-    result = None
-    statusCode = 200
-
     print(event)
-    id = event['params']['path'].get('id', 0)  # coordi_id, product_id,
-    recommend_type = event['params']['querystring'].get('recommendType', None)
-    ids = event['params']['querystring'].get('ids', None)
+    result = None
     userType = event['params']['querystring'].get('userType', None)
     userId = event['params']['querystring'].get('userId', None)
-    result = None
-    statusCode = 200
-    params = {'recommend_type': recommend_type, 'id': id, 'ids': ids, 'user_type': userType, 'user_id': userId}
-    result = realtime_service.get_recommend_realtime_with_item(params.get('user_type'), params.get('user_id'))
+
+    result = realtime_service.get_recommend_realtime_with_item(userType, userId)
     return result
 
 
